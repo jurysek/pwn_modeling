70b2f34630b85ad01fda9adcec6da3d6a737fb5b
jurysek <jakub@MacBook-Pro-4.local>
2024-07-31 02:24:29 +0200

    some minor changes

--- pwn_modeling/utils/time_development.py
+++ pwn_modeling/utils/time_development.py
@@ -10,13 +10,13 @@ def development(power_law_spectrum, age, t, snr, pwn):
     # Setup gamera Particle object
     fp = gp.Particles()
     fp.SetCustomInjectionSpectrum(power_law_spectrum)
-    
+
     # Adding IC photon fields
     # CMB
     u_rad = 4.17 * 10**-13
     temperature = 2.726
-    fp.AddThermalTargetPhotons(temperature, u_rad) #in K, erg/cm^3. 
-    
+    fp.AddThermalTargetPhotons(temperature, u_rad) #in K, erg/cm^3.
+
     # Adding lists of environmental variables for time development
     tim = t.to_value(u.yr)
     b_field = pwn.magnetic_field(t).to("G").value # this should be in Gauss
@@ -25,17 +25,17 @@ def development(power_law_spectrum, age, t, snr, pwn):
     #dt.append(dt[-1])
     v_pwn = pwn.radius(t[:-1]).to("cm").value / dt.to_value(u.s) # in cm/s
     v_pwn = np.append(v_pwn, v_pwn[-1])
-    
+
     # Big question, is this a luminosity, i.e. dE/dt? Shouldn't it be integrated energy in dt?
-    # Gamera tutorials suggest that this is the total energy at time t (in ergs), which does not make much sense to me... 
+    # Gamera tutorials suggest that this is the total energy at time t (in ergs), which does not make much sense to me...
     # It is called luminosity anyway, so I use psr luminosity at time t!
     lum = pwn.pulsar.luminosity_spindown(t).to("erg/s").value
-    
+
     fp.SetLuminosityLookup(list(zip(tim, lum)))
     fp.SetBField(list(zip(tim, b_field)))
     fp.SetExpansionVelocity(list(zip(tim, v_pwn)))
     fp.SetRadius(list(zip(tim, radius)))
-    
+
     fp.SetAge(age) # in yrs
     fp.CalculateElectronSpectrum()
     sp  = np.array(fp.GetParticleSpectrum()) # returns diff. spectrum: E(erg) vs dN/dE (1/erg)
@@ -49,14 +49,14 @@ def development(power_law_spectrum, age, t, snr, pwn):
     #b = pwn.magnetic_field(tim[0]).to("G").value[0]
     b = np.interp(age, tim, b_field)
     fr.SetBField(b)
-    
+
     #fr.SetAmbientDensity(density)
-    distance = 1e3 # optional, in parsec. If not set or equals zero, differential 
+    distance = 1e3 # optional, in parsec. If not set or equals zero, differential
                    # photon production rate instead of flux will be calculated
     fr.SetDistance(distance)
     fr.SetElectrons(sp)
-    # define energies at which gamma-ray emission should be calculated 
-    eg = np.logspace(-6,15,100) * gp.eV_to_erg
+    # define energies at which gamma-ray emission should be calculated
+    eg = np.logspace(3,16,1000) * gp.eV_to_erg
     fr.CalculateDifferentialPhotonSpectrum(eg)
 
     tot = np.array(fr.GetTotalSED())  # SED, E^2dNdE (erg/s/cm^2) vs E (TeV)
@@ -66,7 +66,7 @@ def development(power_law_spectrum, age, t, snr, pwn):
 
     # integrated energy flux (int_e^inf dE E*dN/dE, units: erg / cm^2 / s) times area of the sphere erg/s
     total_radiated_energy = fr.GetIntegralTotalEnergyFlux(eg[0],eg[-1]) * 4 * np.pi * (distance * u.pc).to_value(u.cm)**2
-    
+
     return sp, p_sed, tot, ic, synch, total_radiated_energy
 
 def run_particle_developemnt(power_law_spectrum, age, t, snr, pwn):
